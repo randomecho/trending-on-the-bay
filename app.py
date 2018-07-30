a0bd52f112e39c47f7141aeddb26d78973012193
Soon Van <cog@randomecho.com>
2018-07-30 16:42:32 -0400

    Breathe two newlines around top level functions

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import ebay
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/search")
 def search():
@@ -14,9 +16,11 @@ def search():
 
     return render_template("search.html", keyword=escape(keyword), results=results)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("missing.html"), 404
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
